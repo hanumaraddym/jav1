57137fdf1b885e5cb42584451f89136d308d690a
Hanumaraddy M <hanumarm@amazon.com>
2021-05-08 13:27:35 +0530

    adding dummy

---
+++ src/jab/gi1.java
@@ -0,0 +1,20 @@
+package jab;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class gi1 {
+	public static void main(String[] args) {
+
+		
+		System.setProperty("webdriver.chrome.driver", "/Users/hanumarm/Documents/driver/chromedriver");
+		//WebDriver driver =new ChromeDriver();
+
+		System.out.println("reddy");
+//		
+		
+		//	driver.findElement(By.xpath("//a[contains(text(),'test2 test2')]/parent::td//preceding-sibling::td//input[@name='contact_id']")).click();
+
+	}
+
+}
